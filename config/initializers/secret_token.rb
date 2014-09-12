cc7c44646ab76306cac564a61955be886b841a25
Courtneyjones <me@courtneyejones.com>
2014-09-12 17:56:46 -0400

    Initial Commit
    -Started new rails app
    -created about and contact page
    -created listings and made listings index the homepage through routes
    file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Etsydemo::Application.config.secret_key_base = 'cb98f1cff01ca8f77619970adceb7918fc0860000386b6e862f3a6cd2470c2fae28aaddf00db5738af19b6302d9bab01b9efda1a902d28b97c07154331bad50a'
